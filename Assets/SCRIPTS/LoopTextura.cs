917e62fc101b2d75da38cbce86d850e02beb3cdc
Matias <matiaspierpaoli@gmail.com>
2025-09-22 22:17:53 -0300

    Fixed calibrating and deposit misleading UI indications between platform

--- Assets/SCRIPTS/LoopTextura.cs
+++ Assets/SCRIPTS/LoopTextura.cs
@@ -6,17 +6,24 @@ public class LoopTextura : MonoBehaviour
     public float interval = 1f;
     private float aux = 0f;
 
-    public Sprite[] Images;
+    public Sprite[] ImagesPC;
+    public Sprite[] ImagesMobile;
     private int Contador = 0;
 
+    private Sprite[] currentImages;
     private Image imageComponent;
 
     void Start()
     {
         imageComponent = GetComponent<Image>();
 
-        if (Images.Length > 0 && imageComponent != null)
-            imageComponent.sprite = Images[0];
+        if (GameManager.Instancia.IsPlatformPC())
+            currentImages = ImagesPC;
+        else
+            currentImages = ImagesMobile;
+
+        if (currentImages.Length > 0 && imageComponent != null)
+            imageComponent.sprite = currentImages[0];
     }
 
     void Update()
@@ -27,12 +34,12 @@ public class LoopTextura : MonoBehaviour
         {
             aux = 0f;
             Contador++;
-            if (Contador >= Images.Length)
+            if (Contador >= currentImages.Length)
             {
                 Contador = 0;
             }
             if (imageComponent != null)
-                imageComponent.sprite = Images[Contador];
+                imageComponent.sprite = currentImages[Contador];
         }
     }
 }
--- Assets/SCRIPTS/PantallaCalibTuto.cs
+++ Assets/SCRIPTS/PantallaCalibTuto.cs
@@ -3,19 +3,31 @@ using System.Collections;
 
 public class PantallaCalibTuto : MonoBehaviour
 {
-    public Texture2D[] TutorialImages;
+    public Texture2D[] TutorialImagesPC;
+    public Texture2D[] TutorialImagesMobile;
     public float interval = 1.2f;
     float aux = 0;
     int currentImageIndex = 0;
 
     public bool isPlayer1 = false;
 
-    public Texture2D PCCalibImage;
+    public Texture2D PCCalibImagePlayer1;
+    public Texture2D PCCalibImagePlayer2;
     public Texture2D MobileCalibImage;
 
     public Texture2D ImaReady;
 
     public ContrCalibracion ContrCalib;
+
+    private Texture2D[] currentImages;
+
+    private void Awake()
+    {
+        if (GameManager.Instancia.IsPlatformPC())
+            currentImages = TutorialImagesPC;
+        else
+            currentImages = TutorialImagesMobile;
+    }
 
     void Update()
     {
@@ -25,28 +37,28 @@ public class PantallaCalibTuto : MonoBehaviour
                 if (GameManager.Instancia.IsPlatformPC())
                 {
                     if (isPlayer1)
-                        GetComponent<Renderer>().material.mainTexture = PCCalibImage;
+                        GetComponent<Renderer>().material.mainTexture = PCCalibImagePlayer1;
                     else
-                        GetComponent<Renderer>().material.mainTexture = MobileCalibImage;
+                        GetComponent<Renderer>().material.mainTexture = PCCalibImagePlayer2;
                 }
                 else
                     GetComponent<Renderer>().material.mainTexture = MobileCalibImage;
                 break;
 
             case ContrCalibracion.Estados.Tutorial:
+
                 aux += T.GetDT();
                 if (aux >= interval)
                 {
                     aux = 0;
-                    if (currentImageIndex + 1 < TutorialImages.Length)
+                    if (currentImageIndex + 1 < currentImages.Length)
                         currentImageIndex++;
                     else
                         currentImageIndex = 0;
                 }
-                GetComponent<Renderer>().material.mainTexture = TutorialImages[currentImageIndex];
+                GetComponent<Renderer>().material.mainTexture = currentImages[currentImageIndex];
 
                 break;
-
             case ContrCalibracion.Estados.Finished:
                 GetComponent<Renderer>().material.mainTexture = ImaReady;
                 break;
